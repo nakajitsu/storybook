4dc2fd7a7a449b85a6a6688b07be554c67554d2b
takamasa-satou <takamasa-satou@takamasa-satounoMacBook-Pro-2.local>
2024-03-12 17:25:28 +0900

    1st commit

---
+++ .history/src/components/SampleButton_20240312112909.vue
@@ -0,0 +1,18 @@
+<template>
+  <button type="button" @click="buttonClick">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: "sample-button",
+  props: {
+    label: String,
+  },
+
+  methods: {
+    buttonClick: (e) => {
+      window.alert(`I'm a ${e.target.textContent}`);
+    },
+  },
+};
+</script>
---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from "@storybook/vue3-webpack5";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/vue3-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
